54bd26818eabbe9e47d21d31dc908d5911fedbe3
jmckinny <justinmckinney.md@gmail.com>
2021-03-08 17:43:00 -0500

    still broken :(

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import socket
 import sys
+import select
 class Client:
     def __init__(self, address = socket.gethostname(), port = 1247):
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -11,11 +12,23 @@ class Client:
 
     def send_message(self, msg):
         self.sock.send(msg.encode())
-    
+    def recv_message(self):
+        data = self.sock.recv(1024).decode()
+        return data
     def close(self):
         self.sock.close()
     
 c = Client()
 while True:
-    msg = input(">")
-    c.send_message(msg)
+    inputs = [sys.stdin, c.sock]
+    read_sockets,write_socket, error_socket = select.select(inputs,[],[])
+    for socket in read_sockets:
+        if socket == c.sock:
+            message = c.sock.recv(1024).decode()
+            print(message)
+        else:
+            message = sys.stdin.readline()  
+            c.sock.send(message.encode())
+            sys.stdout.write("<You>")  
+            sys.stdout.write(message)  
+            sys.stdout.flush()  
--- server.py
+++ server.py
@@ -3,38 +3,62 @@ import socket
 import os
 from threading import *
 
+class Client:
+    def __init__(self, socket, address):
+        self.socket = socket
+        self.address = address
+
+
 class Server:
     def __init__(self, port):
         self.sock = socket.socket()
         self.HOST = socket.gethostname()
         self.PORT = os.environ.get("PORT")
+        self.clients = []
         if self.PORT == None:
             self.PORT = 1247
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.HOST,self.PORT))
+
         print(f"Server \"{self.HOST}\" running on port {self.PORT}" )
     
-    def new_client(self, socket, address):
-        Handler(socket, address)
-        
+    def new_client(self,connection,address):
+        client = Client(connection,address)
+        self.clients.append(client)
+        print(f"{address[0]} connected to the server")
+        Thread(target = self.client_thread, args=(client,)).start()
 
-class Handler(Thread):
-    def __init__(self, socket, address):
-        Thread.__init__(self)
-        self.socket = socket
-        self.address = address
-        self.start()
-
-    def run(self):
+    def broadcast(self,message,sender):
+        for client in self.clients:
+            if client != sender:
+                try:
+                    client.socket.send(message)
+                except:
+                    client.socket.close()
+                    self.clients.remove(client)
+    
+    def client_thread(self, client):
+        client.socket.send("Welcome to the chatroom".encode())
         while True:
-            data = self.socket.recv(1024).decode()
-            if not data:
-                break
-            print('Client sent:', data)
-        self.socket.close()
+            try:
+                
+                msg = client.socket.recv(1024).decode()
+                if msg:
+                    message_to_send = f"<{client.address[0]}> {msg}"
+                    print(message_to_send)
+                    self.broadcast(message_to_send,client)
+                else:
+                    print(f"Closing {client.address}")
+                    client.socket.close()
+                    self.clients.remove(client)
+            except:
+                continue
+   
+
 
 s = Server(1247)
 s.sock.listen(5)
 print("Ready and Listening")
 while True:
-    clientsocket, address = s.sock.accept()
-    s.new_client(clientsocket, address)
+    connection, address = s.sock.accept()
+    s.new_client(connection, address)
