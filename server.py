e67eece3591102e6e14f7f717f9f401aab1a3342
jmckinny <justinmckinney.md@gmail.com>
2021-03-10 17:19:48 -0500

    add nick command

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import socket
+import re
 import os
 from threading import *
 
 class Client:
-    def __init__(self, socket, address):
+    def __init__(self, socket, address, name = "anon"):
+        self.name = name
         self.socket = socket
         self.address = address
 
@@ -19,6 +21,7 @@ class Server:
             self.PORT = 1247
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.HOST,self.PORT))
+        self.commands = {"nick": self.nick_command}
 
         print(f"Server \"{self.HOST}\" running on port {self.PORT}" )
     
@@ -41,19 +44,39 @@ class Server:
         client.socket.send("Welcome to the chatroom".encode())
         while True:
             try:
-                
                 msg = client.socket.recv(1024).decode()
                 if msg:
-                    message_to_send = f"<{client.address[0]}> {msg}"
-                    print(message_to_send)
-                    self.broadcast(message_to_send.encode(),client)
+                    if msg[0] == "/":
+                        print("Command: " + msg )
+                        self.command_handler(client,msg)
+                    else:
+                        message_to_send = f"<{client.name}@{client.address[0]}> {msg}"
+                        print(message_to_send)
+                        self.broadcast(message_to_send.encode(),client)
                 else:
                     print(f"Closing {client.address}")
                     client.socket.close()
                     self.clients.remove(client)
             except:
                 continue
-   
+
+    def command_handler(self,client,msg):
+        data = msg[1:].split(" ")
+        command = data[0]
+        args = data[1:]
+        func = self.commands[command]
+        func(client,args)
+
+
+    def nick_command(self,client,args):
+        new_name = re.sub(r"\W+", "", args[0].strip())
+        if len(new_name) > 9:
+            client.socket.send("Name must be 9 chars or less".encode())
+        else:
+            client.name = new_name
+        
+
+
 
 
 s = Server(1247)
