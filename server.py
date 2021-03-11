2c574ac52cc7cb42327142ced3b8500400ce7c91
jmckinny <justinmckinney.md@gmail.com>
2021-03-10 22:38:38 -0500

    add msg command and did some house cleaning

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ class Client:
         self.socket = socket
         self.address = address
 
+    def __str__(self):
+        return f"{self.name}@{self.address[0]}"
 
 class Server:
     def __init__(self, port):
@@ -21,17 +23,19 @@ class Server:
             self.PORT = 1247
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.HOST,self.PORT))
-        self.commands = {"nick": self.nick_command, "who": self.who_command}
-
+        self.commands = {"nick": self.nick_command, "who": self.who_command, "msg": self.msg_command}
+        self.motd = "Welcome to the chat room"
         print(f"Server \"{self.HOST}\" running on port {self.PORT}" )
     
     def new_client(self,connection,address):
         client = Client(connection,address)
         self.clients.append(client)
-        print(f"{address[0]} connected to the server")
+        print(f"{client}:{client.address[1]} connected to the server")
+        join_msg = f"{client} connected to the server"
+        self.broadcast(join_msg.encode(),client)
         Thread(target = self.client_thread, args=(client,)).start()
 
-    def broadcast(self,message,sender):
+    def broadcast(self,message,sender = None):
         for client in self.clients:
             if client != sender:
                 try:
@@ -41,24 +45,31 @@ class Server:
                     self.clients.remove(client)
     
     def client_thread(self, client):
-        client.socket.send("Welcome to the chatroom".encode())
+        client.socket.send(self.motd.encode())
         while True:
             try:
                 msg = client.socket.recv(1024).decode()
                 if msg:
                     if msg[0] == "/":
-                        print("Command: " + msg )
+                        print(f"[{client}] Command: " + msg )
                         self.command_handler(client,msg)
                     else:
-                        message_to_send = f"<{client.name}@{client.address[0]}> {msg}"
+                        message_to_send = f"<{client}> {msg}"
                         print(message_to_send)
                         self.broadcast(message_to_send.encode(),client)
                 else:
-                    print(f"Closing {client.address}")
+                    print(f"Closing {client}:{client.address[1]}")
                     client.socket.close()
                     self.clients.remove(client)
             except:
                 continue
+    
+    def get_client(self,name):
+        for client in self.clients:
+            if client.name == name:
+                return client
+        return None
+
 
     def command_handler(self,client,msg):
         data = msg[1:].split(" ")
@@ -72,6 +83,8 @@ class Server:
         new_name = re.sub(r"\W+", "", args[0].strip())
         if len(new_name) > 9:
             client.socket.send("Name must be 9 chars or less".encode())
+        if self.get_client(new_name) != None:
+            client.socket.send("That name is already taken".encode())
         else:
             self.broadcast(f"{client.name} is now known as {new_name}".encode(),client)
             client.name = new_name
@@ -82,6 +95,16 @@ class Server:
         for c in self.clients:
             result += f"{c.name}@{c.address[0]}\n"
         client.socket.send(result.encode())
+
+    def msg_command(self,client,args):
+        name = re.sub(r"\W+", "", args[0].strip())
+        msg = re.sub(r"\W+", "", args[1].strip())
+        reciever = self.get_client(name)
+        print(reciever)
+        if reciever != None:
+            reciever.socket.send(f"[{client}]: {msg}".encode())
+        else:
+            client.socket.send(f"User {name} does not exist".encode())
 
 
 
